c9e5b74986d214b42e6646d12137a44f96862f9b
nathanhilbert <nathanhilbert@gmail.com>
2016-08-16 11:16:30 -0400

    update raster tests

--- RasterLoadingTests.md
+++ RasterLoadingTests.md
@@ -1,14 +1,16 @@
 Tests for Raster Data Storage in DB
------------------------
+===================
 
 Using landscan data
 
 Options
+-------------
 
- 1. Store Tiled rasters in folder location and register via PG - population.landscan
-   - Add the benefit of allowing other systems to access the tiles
-   - Can quickly do query on PG to find the tile file names and use them in external program
-   - Hard to manage the millions of tiles created with 256x256 layout scheme
+**Postgres**
+ ~~1. Store Tiled rasters in folder location and register via PG - population.landscan~~
+ ~~- Add the benefit of allowing other systems to access the tiles~~
+ ~~- Can quickly do query on PG to find the tile file names and use them in external program~~
+ ~~- Hard to manage the millions of tiles created with 256x256 layout scheme~~
 
  2. Store full raster in file system and register with tiles in PG - population.landscantiled
    - Again, benefit of other programs accessing the file directly
@@ -18,23 +20,51 @@ Options
    - bloated databsae table hard to manage
    - Large amounts of time to make so many transactions
 
-**Not tested**
+**File System Access**
+- Can't take advantage of the related nature of the database system
 
- 4. Store and process everything using stored files?
-   - Can't take advantage of the related nature of the database system
-   - Extra processing and programming time
+ 4. Store in file system with warped source and process with pyspatial
 
+**Geotrellis and Accumulo**
  5. Store arrays in acculumo registered with hadoop with geoserver display?
    - Difficult to ETL
    - Each analytic would have to be custom programmed, but may still be able to take advantage of spatial joins
    - Using geotrellis could help with some of the geo functions
 
 
-**Tests**
+Tests
+--------------------
  - QGIS retrieval and display
-    All. All data displayed at full extent was too slow to be functional.  #2 has the benefit of allowing the user to connect directly to the source or registering that source in a WMS like Geoserver as well.
+    1-3. All data displayed at full extent was too slow to be functional.  #2 has the benefit of allowing the user to connect directly to the source or registering that source in a WMS like Geoserver as well.
+    4. Regular speed for qgis rendering with no speed ups or slowdowns.
+
+
  - Summation query of statistics for all urban clusters in US
+    1018 cities for landscan
+    1.  >20 minutes
+    2.  >20 minutes
+    3.  >20 minutes
+    4. 16.6912670135 seconds
+
+ - Time series 
+    Random polygon in midwest for 20 days in 1980
+    2. Errored Query Run. Not expecting better than 2.5 seconds
+    3. 5 minutes 31 seconds
+    4. 2.51124811172 seconds 
+
+
  - Single stats query of one urban cluster in US
     1. 17.5242609978 seconds
     2. 17.1873991489 seconds
     3. 24.6439399719 seconds
+    4. 14.7944848537 seconds
+
+
+Conclusions
+___________________________
+
+ - Raster storage in Postgis are not adequate for our use of fetching all or even 50 cities.
+ - This was actually the motivation of building the pyspatial package which provides most of the 
+    functionality of postgis in python format.  
+ - Storage of projected and compressed rasters as geotiffs on our file systems with pyspatial and geometries being pulled in from 
+   Postgis seems to be the best solution for now.  
---
+++ RasterLoadingTests/makepyspatialcatalog.py
@@ -0,0 +1,102 @@
+"""
+Copyright (c) 2016, Granular, Inc. 
+All rights reserved.
+License: BSD 3-Clause ("BSD New" or "BSD Simplified")
+Redistribution and use in source and binary forms, with or without modification, are permitted 
+provided that the following conditions are met: 
+  * Redistributions of source code must retain the above copyright notice, this list of conditions 
+    and the following disclaimer.
+  * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the 
+    following disclaimer in the documentation and/or other materials provided with the distribution. 
+  * Neither the name of the nor the names of its contributors may be used to endorse or promote products 
+    derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS 
+OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BE LIABLE FOR ANY DIRECT, 
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+import os
+import argparse
+import json
+from pyspatial.vector import read_layer, read_geojson
+from pyspatial.utils import projection_from_string
+import gdal
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Create json catalog file.')
+    parser.add_argument('src', help='The source raster file, whether tiled '
+                                    'or untiled')
+
+    parser.add_argument('--dest', dest="dest",
+                        help='The output path for the json file',
+                        default=None)
+    parser.add_argument('--tiles', dest='tile_path', default=None,
+                        help='Specify path to tiles')
+
+    parser.add_argument('--index', dest='index_path', default=None,
+                        help='Specify the index file for the grid')
+
+    parser.add_argument('--grid', dest='grid_size', type=int, default=None,
+                        help=('Specify the grid size in pixels '
+                              '(assumes both x and y are the same)'))
+
+    args = parser.parse_args()
+
+    hDataset = gdal.OpenShared(args.src)
+
+    # Get projection.
+    proj = hDataset.GetProjectionRef()
+
+    # Dump to json
+    catalog = {"Path": args.src,
+               "CoordinateSystem": proj,
+               "GeoTransform": hDataset.GetGeoTransform()}
+
+    band = hDataset.GetRasterBand(1)
+    ctable = band.GetColorTable()
+
+    if ctable is not None:
+        colors = [ctable.GetColorEntry(i) for i in range(256)]
+        catalog["ColorTable"] = colors
+
+    xsize = hDataset.RasterXSize
+    ysize = hDataset.RasterYSize
+    catalog["Size"] = (xsize, ysize)
+
+    if args.tile_path is not None:
+        if os.path.exists(args.tile_path):
+            tiles = os.listdir(args.tile_path)
+            if len(tiles) == 0:
+                raise ValueError("%s is empty" % args.tile_path)
+
+            tile = os.path.join(args.tile_path, tiles[0])
+            ds = gdal.OpenShared(tile)
+            if ds is None:
+                raise ValueError("Unable to open file: %s" % tile)
+
+            xsize = ds.RasterXSize
+            ysize = ds.RasterYSize
+            if xsize != ysize:
+                raise ValueError("tiles must have same X and Y size")
+
+            catalog["GridSize"] = xsize
+            catalog["Path"] = args.tile_path
+        else:
+            raise ValueError("tiles path does not exist: %s" % args.tile_path)
+
+    if args.index_path is not None:
+        read = read_geojson if args.index_path.endswith("json") else read_layer
+        index = read(args.index_path)
+        catalog["Index"] = index.transform(projection_from_string()).to_dict()
+
+    if args.dest is not None:
+        with open(args.dest, "w+b") as outf:
+            outf.write(json.dumps(catalog))
+    else:
+        print json.dumps(catalog)
